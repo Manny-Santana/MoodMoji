6fdafa0448857ccfc049d6a2e6012790e19d69fa
Manny-Santana <aresxwarlord@outlook.com>
2019-11-21 14:56:28 -0500

    added basic front end to project

---
+++ front-end/moodmoji-frontend/src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MainContent extends Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>Main Content</h1>
+        <p>all other items will be appended to this </p>
+      </div>
+    );
+  }
+}
+
+export default MainContent;
