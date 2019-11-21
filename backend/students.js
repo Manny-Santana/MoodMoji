3f4e160a944bca9b68347cd3404925a21afb4fc9
Pam Adams <pkadams@epotential.com>
2019-11-21 15:22:57 -0500

    backend

---
+++ backend/students.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const studentSchema = mongoose.Schema({
+    studentName: { type: String, required: true },
+    text: String,
+    emoji: String,
+    parentName: String,
+    teacherId: String
+
+},
+    { timestamps: true })
+
+
+module.exports = mongoose.model("Student", studentSchema)
