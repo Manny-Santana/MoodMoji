3cc9bbe22ddd19ac4736a1cca0ec075868e45b3d
Prathyusha Jaddu <pratz@Prathyushas-MacBook-Air.local>
2019-11-21 15:32:46 -0500

    created users.js & userSeed.js files

---
+++ backend/models/userSeed.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const usersModel = require("./users");
+
+const seedUsersInfo = [
+    {
+        email: "pam@gmail.com",
+        password: "pam"
+    },
+    {
+        email: "perrin@gmail.com",
+        password: "perrin"
+    },
+    {
+        email: "manny@gmail.com",
+        password: "manny"
+    },
+    {
+        email: "pratz@gmail.com",
+        password: "pratz"
+    }
+];
+   
+// Seeding function
+const seedDB = () => {
+    // Declare db name, URI, and instantiate connection
+    // =======================================
+    //              DATABASE
+    // =======================================
+    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/studentDB'
+    
+    mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true
+    });
+    mongoose.connection.once("open", () => {
+      console.log("connected to mongo");
+    });
+
+    //Delete database collection seed data
+usersModel.collection.drop();
+
+usersModel.create(seedUsersInfo, (err, newItems) => {
+    if (err) {
+      console.log('Seeding error: ', err);
+    } else {
+      console.log('Seeding OK: ', newItems);
+    }
+    dbConnection.close();
+  });
+}
+
+seedDB();
+
+module.exports = seedUsersInfo
