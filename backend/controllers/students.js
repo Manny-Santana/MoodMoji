f18c847207cc58af1365ba20b0ba8c5ce8cde5b2
Prathyusha Jaddu <pratz@Prathyushas-MacBook-Air.local>
2019-11-21 16:49:06 -0500

    created server.js & index,post routes

---
+++ backend/controllers/students.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const students = express.Router();
+const Student = require('../models/students.js');
+
+//POST ROUTE
+students.post('/', async (req, res) => {
+    Student.create(req.body, (error, createdStudent) => {
+      if (error) {
+        res.status(400).json({ error: error.message })
+      }
+      res.status(200).send(createdStudent) 
+    })
+  })
+
+//INDEX ROUTE
+students.get('/', (req, res) => {
+    Student.find({}, (err, foundStudents) => {
+      if (err) {
+        res.status(400).json({ error: err.message })
+      }
+      res.status(200).json(foundStudents)
+    })
+})
+
+module.exports = students;
