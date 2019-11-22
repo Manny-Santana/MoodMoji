5b002c6a18a8ea236b8d772da953d5bc34de637b
Manny-Santana <aresxwarlord@outlook.com>
2019-11-22 14:13:45 -0500

    rendered student feeling data

--- front-end/moodmoji-frontend/src/App.js
+++ front-end/moodmoji-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MainContent from "./components/MainContent";
+import Student from "./components/Students";
 
 function App() {
   return (
     <div className="App">
       <MainContent />
+      
     </div>
   );
 }
