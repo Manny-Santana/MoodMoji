93453169660f031335ea7b26a2481998773cc155
Pam Adams <pkadams@epotential.com>
2019-11-25 10:04:19 -0500

    backend and frontend

--- front-end/moodmoji-frontend/src/App.js
+++ front-end/moodmoji-frontend/src/App.js
@@ -1,52 +1,139 @@
 import React from "react";
+import Table from "./components/Table";
 import "./App.css";
-import MainContent from "./components/MainContent";
 import CreateForm from "./components/CreateForm";
+import axios from "axios";
+
+let baseURL = process.env.REACT_APP_BASEURL;
+
+if (process.env.NODE_ENV === "development") {
+  baseURL = "http://localhost:3003";
+} else {
+  baseURL = "";
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      emojis: [],
-      emoji: {}
+      students: [
+        {
+          id: 1,
+          studentName: "Ted",
+          text: "sleepy",
+          mood: ":worried:",
+          parentName: "Susan",
+          teacherId: "Elaine"
+        },
+        {
+          id: 2,
+          studentName: "Bobby",
+          text: "Did not eat breakfast",
+          mood: ":confused:",
+          parentName: "John",
+          teacherId: "Elaine"
+        },
+        {
+          id: 3,
+          studentName: "Albert",
+          text: "ready for school",
+          mood: ":expressionless:",
+          parentName: "June",
+          teacherId: "Elaine"
+        },
+        {
+          id: 4,
+          studentName: "John",
+          text: "happy",
+          mood: ":grinning:",
+          parentName: "Sandy",
+          teacherId: "Elaine"
+        },
+        {
+          id: 5,
+          studentName: "Joan",
+          text: "looking forward to the halloween party",
+          mood: ":smile:",
+          parentName: "Henry",
+          teacherId: "Elaine"
+        }
+      ],
+      student: {}
     };
-    // this.deleteMood = this.deleteMood.bind(this);
+    this.getStudents = this.getStudents.bind(this);
+    // this.getStudent = this.getStudent.bind(this);
+    this.removeStudent = this.removeStudent.bind(this);
+    this.setState = this.setState.bind(this);
   }
 
   //Function tied CreateForm to add a new Mood
-  handleAddMood(emoji) {
+  handleAddMood(student) {
     this.setState({
-      emojis: [...this.state.emojis, emoji]
+      students: [...this.state.students, student]
     });
   }
 
-  // async deleteMood(id) {
-  //   await axios.delete(`${baseURL}/bookmarks/${id}`);
-  //   const filteredMoods = this.state.moods.filter(mood => {
-  //     return mood._id !== id;
+  componentDidMount() {
+    this.getStudents();
+  }
+
+  async getStudents() {
+    const response = await axios(`${baseURL}`);
+    this.setState([...this.state.students, response.data]);
+  }
+
+  setMood = event => {
+    const { student, id } = event.target;
+    console.log(id);
+
+    this.setState({
+      [student]: id
+    });
+  };
+
+  // async removeStudent(id) {
+  //   await axios.delete(`${baseURL}/students/${id}`);
+  //   const filteredFoundStudents = this.state.foundStudents.filter(student => {
+  //     return student._id !== id;
   //   });
 
   //   this.setState({
-  //     moods: filteredMoods
+  //     students: filteredFoundStudents
   //   });
   // }
+
+  removeStudent = index => {
+    const { students } = this.state;
+
+    this.setState({
+      students: students.filter((student, i) => {
+        return i !== index;
+      })
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <CreateForm handleAddMood={this.handleAddMood} />
-        <ul style={{ listStyle: "none" }}>
-          {/* {this.props.moods.map(item => {
-            <li>
-              Emoji: {item.emoji}
-              Date:
-              <button onClick={() => this.deleteMood(bookmark._id)}>
-                DELETE
-              </button>
-            </li>;
-          })} */}
-        </ul>
-        <MainContent />
+        <h1>How is your child feeling today?</h1>
+        <CreateForm addStudent={this.addStudent} />
+        {/* <ul>
+          {this.state.students.map(item => {
+            return (
+              <li>
+                emoji: {item.emoji}
+                <button onClick={() => this.deleteMood(item._id)}>
+                  DELETE
+                </button>
+              </li>
+            );
+          })}
+        </ul> */}
+        <Table
+          studentData={this.state.students}
+          removeStudent={this.removeStudent}
+          setMood={this.state.setMood}
+        />
       </div>
     );
   }
